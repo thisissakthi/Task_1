03c0ef8747b9464ed6710f284e2074359c9c3fb8
thisissakthi <mspprasath1133@gmail.com>
2020-12-10 11:18:19 +0530

    heroku

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ const knex=require('knex')({
   client:'pg',
   connection: 'postgres://ocdlnqxr:3jP9oxE48hddqlbOsinSvMO33z08iITI@isilo.db.elephantsql.com:5432/ocdlnqxr'
 });
-<<<<<<< HEAD
-/* connected to online DataBase */
 const app=express();
 app.use(bodyParser.json());
+//<<<<<<< HEAD
+/* connected to online DataBase */
 
-/*knex.schema.createTable('user', (table) => {
-=======
+
+//knex.schema.createTable('user', (table) => {
+//=======
 //connecting to online database
 
 const app=express();
 app.use(bodyParser.json());
 
 //creating table for one time
-knex.schema.createTable('user', (table) => {
->>>>>>> 6b46f3135a3751f9d64981018ca71055b9c8e280
-  table.increments('id')
-  table.string('data')
-}).then(()=>
-{
-  console.log("Table Created");
-})
-.catch((err)=>{
-  console.log(err);
-})
-Initially created to table
-*/
+//knex.schema.createTable('user', (table) => {
+//  table.increments('id')
+  //table.string('data')
+//}).then(()=>
+//{
+  //console.log("Table Created");
+//})
+//.catch((err)=>{
+//  console.log(err);
+//})
+//Initially created to table
+
 
 //get request to fetch data's
 app.get('/',(req,res)=>{
